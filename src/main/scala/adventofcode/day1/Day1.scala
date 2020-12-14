24ead9a81837155813c9f83bd7b6c8ec8d5f1d55
adampridmore <adam_pridmore@hotmail.com>
2020-12-14 19:10:37 +0000

    Day1 - Solution part 1

--- src/main/scala/adventofcode/day1/Day1.scala
+++ src/main/scala/adventofcode/day1/Day1.scala
@@ -1,16 +1,15 @@
 package adventofcode.day1
 
 object Day1 extends App {
-  def solve(lines: String) : Int = {
-    
-    val expenses = lines
-      .split(System.lineSeparator())
-      .map(line => line.trim.toInt)
+  def solve(lines: Seq[String]) : Int = {
+  val expenses = lines.map(line => line.trim.toInt)
 
     val m = getMatch(expenses.head, expenses.tail)
 
     m._1 * m._2
   }
+
+  def solve(lines: String) : Int =  solve(lines.split(System.lineSeparator()))
 
   def getMatch(expense: Int, rest: Seq[Int]) : (Int, Int) = {
     rest
--- src/test/scala/adventofcode/Day1Spec.scala
+++ src/test/scala/adventofcode/Day1Spec.scala
@@ -5,11 +5,12 @@ import org.scalatest.funsuite._
 import adventofcode.day1.Day1
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
+import scala.io.Source
 // import org.scalatest.wordspec.WordSpec
 // import org.scalatest.Matchers.should
 
 class Day1Spec extends AnyWordSpec with Matchers {
-  "expenses" should {
+  "expenses example" should {
     "be 514579" in {
       val expenseLines = """1721
   979
@@ -20,5 +21,11 @@ class Day1Spec extends AnyWordSpec with Matchers {
 
       Day1.solve(expenseLines) shouldBe 514579
     }
+  }
+
+  "real file" in {
+    val filename = "day1/expenses.txt"
+    val lines = Source.fromResource(filename).getLines.toSeq
+    println("Day 1 : " + Day1.solve(lines))
   }
 }
