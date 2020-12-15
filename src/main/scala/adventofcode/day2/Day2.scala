d597084f7cb9f6d06fcd9ba75df3d188af6a5f06
adampridmore <adam_pridmore@hotmail.com>
2020-12-15 21:11:43 +0000

    Day2 - Part II: Used a partial function

--- src/main/scala/adventofcode/day2/Day2.scala
+++ src/main/scala/adventofcode/day2/Day2.scala
@@ -4,38 +4,29 @@ import scala.util.matching.Regex
 
 object Day2 extends App {
 
-  def solve(lines: Seq[String]) : Int = {  
-    def isValid(min: Int, max: Int, letter: Char, password: String) : Boolean = {
-      val passwordLetterCount = password.count(c => c == letter)
-      
-      passwordLetterCount >= min && passwordLetterCount <= max
-    }
+  private def countValidPasswords(isValid: (Int, Int, Char, String) => Boolean)(lines: Seq[String]) : Int = {
+    val pattern = "([0-9]+)-([0-9]+) (\\w): (\\w+)".r
 
-    val Pattern = "([0-9]+)-([0-9]+) (\\w): (\\w+)".r
-
-    lines.map(line => {
-        line match {
-          case Pattern(a,b,c,d) => (a.toInt, b.toInt, c.toCharArray.head ,d)
-        }
-    })
-    .count( x => isValid(x._1, x._2, x._3, x._4) )
+    lines
+      .map(_ match {
+          case pattern(a,b,c,d) => (a.toInt, b.toInt, c.toCharArray.head ,d)
+        })
+      .count( x => isValid(x._1, x._2, x._3, x._4) )
   }
 
-  def solve2(lines: Seq[String]) : Int = {
-     def isValid(i: Int, j: Int, letter: Char, password: String) : Boolean = {
+  def solve : (Seq[String]) => Int = {  
+    countValidPasswords((min: Int, max: Int, letter: Char, password: String) => {
+      val count = password.count(c => c == letter)    
+      count >= min && count <= max
+    })
+  }
+
+  def solve2 : (Seq[String]) => Int = {
+    countValidPasswords((i: Int, j: Int, letter: Char, password: String) => {
       (password(i-1) == letter, password(j-1) == letter) match {
         case (true, false) | (false, true) => true
         case _ => false
       }
-    }
-
-    val Pattern = "([0-9]+)-([0-9]+) (\\w): (\\w+)".r
-
-    lines.map(line => {
-        line match {
-          case Pattern(a,b,c,d) => (a.toInt, b.toInt, c.toCharArray.head ,d)
-        }
     })
-    .count( x => isValid(x._1, x._2, x._3, x._4) )
   }
 }
