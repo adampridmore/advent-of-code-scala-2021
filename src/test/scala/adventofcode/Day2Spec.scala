5ca674e459105ed38dbf0d971ebe47c648528e4a
Adam Pridmore <adam_pridomre@hotmmail.com>
2021-12-07 21:25:34 +0000

    Solve day 2 part 2

--- src/test/scala/adventofcode/Day2Spec.scala
+++ src/test/scala/adventofcode/Day2Spec.scala
@@ -15,50 +15,104 @@ object Position {
   val start = Position(0,0)
 }
 
+case class PositionPart2(horizontal: Int, depth: Int, aim: Int)
+
+object PositionPart2 {
+  val start = PositionPart2(0,0,0)
+}
+
 class Day2Spec extends AnyWordSpec with Matchers with AdventOfCodeHelper{
   override def inputFilename(): String = "day2/input.txt"
-  
-  def solve(inputText: String) : Int = {
+  case class Command(text: String, ammount: Int)
 
-    var lines = inputText
+  def getCommands(inputText : String) : Seq[Command] = {
+    inputText
       .split(System.lineSeparator())
       .filter(!_.isBlank())
       .map(_.trim())
+      .map(line => {
+        val bits = line.split(" ")
+        Command(bits(0), bits(1).toInt)
+      })
+  }
+   
+  "part 1" should {
+    def solve(inputText: String) : Int = {
 
-    def next(currentPosition: Position, line: String) : Position = {
-      val commmand = line.split(" ")(0)
-      val ammount = line.split(" ")(1).toInt
+      val lines = getCommands(inputText)
 
-      commmand match {
-        case "down" => currentPosition.copy(depth = currentPosition.depth + ammount)
-        case "up" => currentPosition.copy(depth = currentPosition.depth - ammount)
-        case "forward" => currentPosition.copy(horizontal = currentPosition.horizontal + ammount)
+      def next(currentPosition: Position, command: Command) : Position = {
+        (command.text) match {
+          case "down" => currentPosition.copy(depth = currentPosition.depth + command.ammount)
+          case "up" => currentPosition.copy(depth = currentPosition.depth - command.ammount)
+          case "forward" => currentPosition.copy(horizontal = currentPosition.horizontal + command.ammount)
+        }
       }
+      
+      val finalPosition = lines.foldLeft(Position.start)(next)
+    
+      finalPosition.depth * finalPosition.horizontal
     }
-    
-    val finalPosition = lines.foldLeft(Position.start)(next)
-    
-    println(finalPosition)
 
-    finalPosition.depth * finalPosition.horizontal
+    "example" in {
+      var inputText = """forward 5
+      down 5
+      forward 8
+      up 3
+      down 8
+      forward 2"""
+
+      val answer = solve(inputText)
+      println(answer)
+      answer shouldBe (10 * 15)
+    }
+
+    "Solution" in {
+      val answer = solve(data)
+      println(answer)
+      answer shouldBe 2102357
+    }
   }
+  
+  "part 2" should {
+    def solvePart2(inputText: String) : Int = {
 
-  "part 1 example" in {
-    var inputText = """forward 5
-    down 5
-    forward 8
-    up 3
-    down 8
-    forward 2"""
+      val lines = getCommands(inputText)
 
-    val answer = solve(inputText)
-    println(answer)
-    answer shouldBe (10 * 15)
-  }
+      def next(currentPosition: PositionPart2, command: Command) : PositionPart2 = {
+        (command.text) match {
+          case "down" => currentPosition.copy(aim = currentPosition.aim + command.ammount)
+          case "up" => currentPosition.copy(aim = currentPosition.aim - command.ammount)
+          case "forward" => {
+            currentPosition.copy(
+              horizontal = currentPosition.horizontal + command.ammount,
+              depth = currentPosition.depth + (currentPosition.aim*command.ammount))
+          }
+        }
+      }
+      
+      val finalPosition = lines.foldLeft(PositionPart2.start)(next)
+    
+      finalPosition.depth * finalPosition.horizontal
+    }
 
-  "Part 1" in {
-    val answer = solve(data)
-    println(answer)
-    answer shouldBe 2102357
+    "example" in {
+      var inputText = """forward 5
+      down 5
+      forward 8
+      up 3
+      down 8
+      forward 2"""
+
+      val answer = solvePart2(inputText)
+      println(answer)
+      answer shouldBe (15 * 60)
+    }
+
+    "Solution" in {
+      val answer = solvePart2(data)
+      println(answer)
+      answer shouldBe 2101031224
+    }
   }
 }
