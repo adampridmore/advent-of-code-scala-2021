40429e5426c2e661cda7807e9eb33a1f405ddbb4
Adam Pridmore <adam_pridomre@hotmmail.com>
2021-12-09 22:54:37 +0000

    Tidy imports

--- src/test/scala/adventofcode/Day1Spec.scala
+++ src/test/scala/adventofcode/Day1Spec.scala
@@ -1,11 +1,7 @@
 package adventofcode
 
-import adventofcode._
-import org.scalatest.funsuite._
-import adventofcode.day1.Day1
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
-import scala.io.Source
 
 class Day1Spec extends AnyWordSpec with Matchers with AdventOfCodeHelper {
   val inputFilename = "day1/input.txt"
--- src/test/scala/adventofcode/Day3Spec.scala
+++ src/test/scala/adventofcode/Day3Spec.scala
@@ -1,13 +1,7 @@
 package adventofcode
 
-import adventofcode._
-import org.scalatest.funsuite._
-import adventofcode.day1.Day1
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
-import scala.io.Source
-import adventofcode.day1.Day2
-import scala.util.matching.Regex
 import java.math.BigInteger
 
 class Day3Spec extends AnyWordSpec with Matchers with AdventOfCodeHelper{
